6867fdf5850e0de525c3120df60675cfe49cb55b
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-13 15:41:03 +0300

    methods in classes

---
+++ methods/methods.py
@@ -0,0 +1,9 @@
+# An class of an animal piglet
+class Piglet:
+    # define a method to make some sound
+    def speak(self):
+        print("Oink! Oink!")
+# Creating an object of the class Piglet
+hamlet = Piglet()
+# Call the object
+hamlet.speak()
